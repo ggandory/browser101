f1ad51d2671c0ccc9ca6a380e5f550873b2eea51
허건 <braveman0823@gmail.com>
2024-04-04 23:18:40 +0900

    좌표찾아007

---
+++ .history/chapter3/coordinates/main_20240404230714.js
@@ -0,0 +1,17 @@
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizotal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  console.log(`${x} ${y}`);
+
+  //여기서 왜 left랑 top으로 기준을 잡은거임? 선을
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+});

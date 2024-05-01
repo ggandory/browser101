568ced88d89bb81cf01d36512dc6607c947a8e2b
허건 <braveman0823@gmail.com>
2024-05-01 23:42:51 +0900

    게임만들기 #1

---
+++ chapter7/carrot/main.js
@@ -0,0 +1,7 @@
+const field = document.querySelector(".game_field");
+const fieldRect = field.getBoundingClientRect();
+
+function initGame() {
+  //벌레와 당근을 생성한뒤 field에 추가해준다.
+  console.log(fieldRect);
+}
